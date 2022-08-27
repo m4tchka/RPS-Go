eb3f3a852863f79061ad570d07d4323c02b55a57
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-08-27 22:05:20 +0100

    Completed project - Added line-by-line comments

--- main.go
+++ main.go
@@ -14,18 +14,19 @@ func getPlayerName() string {
 	//Print the string above to console
 	fmt.Scan(&playerName)
 	//Takes whatever the user types into the console and presses enter on, and assigns it as the value associated with the address of the variable, playerName.
+	//NB empty strings not accepted - will only move on once at least 1 character is entered
 	return playerName
-	//Return the variable
+	//Return the playerName variable
 }
 func getPlayerMove() string {
 	var playerInput, playerMove string
 	//Declare 2 string variables
-	fmt.Print("Please input a move, or type stop to end the game: ")
+	fmt.Print("Input a move: ")
 	//Print the string above to console
 	fmt.Scan(&playerInput)
-	//Takes the user's input and (effectively) assigns it to variable playerInput
+	//Takes the user's input and (effectively) assigns it to the variable playerInput
 	playerInput = strings.ToLower(playerInput)
-	//Lowercase the variable playerInput
+	//Turn the player's input into lowercase
 	if playerInput != "" {
 		switch playerInput {
 		case "rock":
@@ -40,12 +41,16 @@ func getPlayerMove() string {
 			fmt.Println("Invalid move!")
 		}
 	}
+	// Assign the player's input to the variable playerMove
+	// If the input is not a valid move,  print the string "Invalid move!" and reprompt
 	return playerMove
 }
 func getComputerMove() string {
 	var computerMove string
 	rand.Seed(time.Now().UnixNano())
+	// Changes RNG seed to be current time at function call from Unix time, in nanoseconds
 	number := rand.Intn(3)
+	// Rolls an INTEGER between 0 (inclusive) and 3 (exclusive) - A half-open (or half-closed) interval of numbers, notation: [0, n)
 	switch number {
 	case 0:
 		computerMove = "rock"
@@ -57,15 +62,18 @@ func getComputerMove() string {
 		computerMove = "scissors"
 		fmt.Printf("The computer chose %v. \n", computerMove)
 	}
+	// Assign a move to computerMove according to the number rolled, and print it to the terminal. Then return said move
 	return computerMove
 }
 func findResult(move, cMove string) string {
 	var result string
 	if move == "stop" {
 		result = "Stopping..."
+		// First check if the move was "stop" - assign the result variable the string "Stopping..."" (which is printed in the console by line 101 when findResult is called)"
 	} else if move == cMove {
 		result = "Draw"
 		playerD++
+		// Next, check if the player's move matches the computer's random move - if it is, increment the draw count
 	} else if (move == "rock" && cMove == "scissors") ||
 		(move == "paper" && cMove == "rock") ||
 		(move == "scissors" && cMove == "paper") {
@@ -76,32 +84,37 @@ func findResult(move, cMove string) string {
 		(move == "scissors" && cMove == "rock") {
 		result = "Computer wins!"
 		playerL++
+		// 2 x else if blocks, checking who won and incrementing the score accordingly; they may only be triggered if the move was "rock" or "paper" or "scissors"
 	} else {
-		fmt.Println("Please input a valid move: rock, paper or scissors,")
-		fmt.Println("or type 'stop' to exit the game !")
+		result = "Please input a valid move: rock, paper or scissors, or type 'stop' to exit the game !"
 	}
+	// To get to this else statement, all the other checks must have been false - therefore the move is invalid !
+	// Assign to the result variable a string telling the user the valid inputs (which is printed in the console by line 101 when findResult is called), and does not increment the score.
 	return result
 }
 
 var playerW, playerL, playerD int
 
+// Declare score-keeping variables
+
 func main() {
 	var name, move, cMove string
 	name = getPlayerName()
+	// Assign the returned variable from getPlayerName to the variable name
 	fmt.Printf("Greetings, %v. \n", name)
+	// Greet the player
 	for move != "stop" {
+		// Whilst the move variable is not the string "stop", do the following:
 		move, cMove = getPlayerMove(), getComputerMove()
+		// Assign the move and cMove variables to be the values returned from the 2 functions above
 		fmt.Println(findResult(move, cMove))
+		// Print the result of handing the previous variables to the findResult function
 		fmt.Printf("Current score for %v is: Wins: %d -- Draws %d -- Losses: %d \n", name, playerW, playerD, playerL)
+		// Print the current score for the player
 	}
 	if move == "stop" {
+		// If the move variable does have the value "stop" then:
 		fmt.Printf("Have a good day, %v!", name)
+		// Say a farewell to the player, and conclude the function main function, exiting the program !
 	}
-
 }
-
-// random string input
-// if move = stop, stop
-// if move = cmove, draw
-// if move = r/p/s & cmove = s/r/p, win/lose (and v,v)
-// else reprompt
